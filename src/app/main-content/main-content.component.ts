aea5347e9ff572335278192d07185f7b6ad324c6
AgaPoli <apolichnowska@tlen.pl>
2019-06-14 15:37:49 +0200

    Create theme.css, update angular.json, setting.json , components files

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-content.component.scss']
 })
 export class MainContentComponent implements OnInit {
-
+movies = [
+'movie1',
+'movie2',
+'movie3',
+'movie4',
+'movie5',
+'movie6',
+'movie7',
+'movie8',
+'movie9',
+'movie10',
+'movie11'
+];
   constructor() { }
 
   ngOnInit() {
