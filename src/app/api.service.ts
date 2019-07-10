923ff5ddaffa25b948253ccecc5969553641cf2e
AgaPoli <apolichnowska@tlen.pl>
2019-07-10 10:58:13 +0200

    Create api.service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,11 +9,31 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) {}
 
+  getImagePath(posterPath: string): string{
+    return environment.urlImageApi+posterPath;
+  }
+
   getPopular(): Promise<any>{
     // https://api.themoviedb.org/3/movie/popular?api_key=c55603a6d0320ce9d6f18ef4abb3932b
 
     return this.httpClient.get(
       `${environment.urlApi}/3/movie/popular`,
+      {
+        params:{ 
+          api_key: environment.apiKey
+        }
+      }
+    ).toPromise()
+    .then(data => data)
+    .catch(error => {console.log(error)})
+
+  }
+
+  getMovie(movie_id: number): Promise<any>{
+    // https://api.themoviedb.org/3/movie/429617?api_key=c55603a6d0320ce9d6f18ef4abb3932b
+
+    return this.httpClient.get(
+      `${environment.urlApi}/3/movie/${movie_id}`,
       {
         params:{ 
           api_key: environment.apiKey
