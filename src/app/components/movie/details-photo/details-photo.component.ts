847a3dbe9170357ee9b82d79687c7c1da8edd02c
AgaPoli <apolichnowska@tlen.pl>
2019-09-24 12:22:53 +0200

    Modify  getImbdLink

--- src/app/components/movie/details-photo/details-photo.component.ts
+++ src/app/components/movie/details-photo/details-photo.component.ts
@@ -6,35 +6,16 @@ import { Rating } from 'src/app/models/movie.model';
   templateUrl: './details-photo.component.html',
   styleUrls: ['./details-photo.component.scss']
 })
-export class DetailsPhotoComponent implements OnInit {
 
-  @Input() src: string;
-  @Input() rating: Rating;
+/**
+ * 
+ */
+export class DetailsPhotoComponent {
+  @Input() 
+  src: string;
 
-  constructor() { }
+  @Input() 
+  rating: Rating;
 
-  ngOnInit() {
-    console.log(this.src);
-  }
-
-  getImbdLink(imdbId: string): string|boolean{
-    const imbdLink = `https://www.imdb.com/title/${imdbId}`;
-    // const OLD_imbdLink = 'https://www.imdb.com/title/'+imdbId;
-    
-    // "5": string !== 5: number   //  true
-    // "5": string != 5: number   //  false
-    // "5": string == 5: number   //  true
-    // "5": string === 5: number  // false
-    // "5": string === `5`: string  // true
-    // "5": string === '5': string  // true
-
-    if (imdbId !== ""){
-      return imbdLink;
-    }else{
-      return false;
-    }
-    
-    return imdbId !== "" ? imbdLink : false;
-  }
-
+  imdBase: string = 'https://www.imdb.com/title/';
 }
