7a6e665a00767eeba9d82f4ec8f4cfb8ba8304bc
AgaPoli <apolichnowska@tlen.pl>
2019-10-01 11:06:22 +0200

    Update API for Git

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  urlApi: "fake-api.pl"
+  urlApi: 'https://api.themoviedb.org',
+  urlImageApi: 'https://image.tmdb.org/t/p/w500/',
+  apiKey: '45602a245709e3381f14ab5e3927f7eb'
 };
